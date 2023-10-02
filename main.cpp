f1ae9e7bb25114a2b4445c876268b737be4b7a01
SC-One <heydar.mahmoodi75@gmail.com>
2023-10-02 16:39:06 +0330

    Added frameless

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
 #include "TestSliderMac.h"
 
 #include <QApplication>
+#include <Thirdparty/NiceFramelessWindow/framelesswindow/framelesswindow.h>
 
 int main(int argc, char *argv[]) {
   QApplication a(argc, argv);
-  TestSliderMac w;
-  w.show();
+  CFramelessWindow window;
+  auto x = new TestSliderMac(&window);
+  window.setCentralWidget(x);
+//  w.show();
+  window.setGeometry(100,100,770,500);
+  window.show();
   return a.exec();
 }
